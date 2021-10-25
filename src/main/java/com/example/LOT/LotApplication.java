f37774f4a6741a060e92b86c94270c8ce48c3835
Tomek <bator.tomasz@gmail.com>
2021-10-25 21:15:32 +0200

    Initial commit

---
+++ src/main/java/com/example/LOT/LotApplication.java
@@ -0,0 +1,13 @@
+package com.example.LOT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotApplication.class, args);
+	}
+
+}
