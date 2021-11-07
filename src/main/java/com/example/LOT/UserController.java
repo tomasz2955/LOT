ab6ad8de52cd124c78dcfd0b1f025b171b174cfa
piotrkosinski1992@o2.pl <piotrkosinski1992@o2.pl>
2021-11-07 11:36:27 +0100

    code review

--- src/main/java/com/example/LOT/UserController.java
+++ src/main/java/com/example/LOT/UserController.java
@@ -1,19 +1,21 @@
 package com.example.LOT;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping
 public class UserController {
 
     private final UserService userService;
 
-    @Autowired
+    //@Autowired adnotacja nie potrzebna, jak wstrzykujesz przez konstruktor to spring to sam ogarnia
     public UserController(UserService userService) {
         this.userService = userService;
     }
--- src/main/java/com/example/LOT/UserService.java
+++ src/main/java/com/example/LOT/UserService.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000")
-@RestController
+@RestController // to jest controller czy @Service? Aplikacja nie startuje
 public class UserService {
     //przydalby sie endpoint do logowania ktory moglby przy poprawnym logowaniu zwracac date jak dlugo logowanie powinno byc wazne
 
@@ -22,31 +22,32 @@ public class UserService {
         return userRepository.findAll();
     }
 
-    @PostMapping("/create/{user}")
+    @PostMapping("/create/{user}") // musimy pogadać o koncepcji DTO https://www.baeldung.com/java-dto-pattern
     User createUser(@RequestBody User user) {
         return userRepository.save(user);
     }
 
-    @PostMapping("/login/{user}")
-    void loginUser(@RequestBody User email, @PathVariable User password) {
+    @PostMapping("/login/{user}") // czy sprawdzales czy te endpointy działają przez postmana? Musimy pogadać o tym jak działa dodawanie parametrów do sciezki
+    void loginUser(@RequestBody User email, @PathVariable User password) { //typy email i password są niepoprawne
 
     }
 
-    @DeleteMapping("/delete/{user}")
-    public List<User> removeUser(@PathVariable User user) {
+    @DeleteMapping("/delete/{user}")  // powinienes odbierac samo id a nie calego Usera
+    public List<User> removeUser(@PathVariable User user) {  // musimy obgadac roznice pomiedzy @RequestBody vs @PathVariable
+        // https://www.baeldung.com/spring-request-response-body  https://www.baeldung.com/spring-pathvariable
         userRepository.delete(user);
-        return userRepository.findAll();
+        return userRepository.findAll();  // po co zwracac wszystkich uzytkownikow podczas update jednego?
     }
 
     @PutMapping("/update/{user}")
     public List<User> updateUser(@RequestBody User user, @PathVariable Long id) {
-        User currentuser = userRepository.findById(id).get();
+        User currentuser = userRepository.findById(id).get(); //w momencie braku odnalezionego id, trzeba rzucic exception
         currentuser.setName(user.getName());
         currentuser.setLastName(user.getLastName());
-        currentuser.setEmail(user.getEmail());
+        currentuser.setEmail(user.getEmail()); // update bardzo naiwny, ktos moze podac tutaj niepoprawny email, email ktory juz istnieje itp. to trzeba jakos validowac
         currentuser.setPhoneNumber(user.getPhoneNumber());
         currentuser.setPassword(user.getPassword());
-        return userRepository.findAll();
+        return userRepository.findAll(); // po co zwracac wszystkich uzytkownikow podczas update jednego?
     }
 
 
